1e44f647e5505cac8caf72cafe0162acd1f4a15f
Shahbaz <sc@shahbazc.com>
2023-09-27 14:50:40 +0100

    created subreddits

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import Navbar from '@/components/Navbar';
+import Providers from '@/components/Providers';
 import { Toaster } from '@/components/ui/toaster';
 import { cn } from '@/lib/utils';
 import '@/styles/globals.css';
@@ -28,16 +29,18 @@ export default function RootLayout({
 			)}
 		>
 			<body className='min-h-screen pt-12 bg-gray-950 antialiased'>
-				{/* @ts-expect-error server component */}
-				<Navbar />
+				<Providers>
+					{/* @ts-expect-error server component */}
+					<Navbar />
 
-				{authModal}
+					{authModal}
 
-				<div className='container max-w-7xl mx-auto h-full pt-12'>
-					{children}
-				</div>
+					<div className='container max-w-7xl mx-auto h-full pt-12'>
+						{children}
+					</div>
 
-				<Toaster />
+					<Toaster />
+				</Providers>
 			</body>
 		</html>
 	);
