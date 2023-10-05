98aa57ba8b5ddc31440aef7a9d8de0fb5ebc3a8b
Shahbaz <sc@shahbazc.com>
2023-10-05 18:56:07 +0100

    display posts in home feed

---
+++ src/components/CustomFeed.tsx
@@ -0,0 +1,44 @@
+import { PAGINATION_RESULTS } from '@/config';
+import { db } from '@/lib/db';
+import PostFeed from './PostFeed';
+import { getAuthSession } from '@/lib/auth';
+import { notFound } from 'next/navigation';
+
+const CustomFeed = async () => {
+	const session = await getAuthSession();
+
+	if (!session) return notFound();
+
+	const followedCommunities = await db.subscription.findMany({
+		where: {
+			userId: session.user.id,
+		},
+		include: {
+			subreddit: true,
+		},
+	});
+
+	const posts = await db.post.findMany({
+		where: {
+			subreddit: {
+				name: {
+					in: followedCommunities.map(({ subreddit }) => subreddit.id),
+				},
+			},
+		},
+		orderBy: {
+			createdAt: 'desc',
+		},
+		include: {
+			votes: true,
+			author: true,
+			comments: true,
+			subreddit: true,
+		},
+		take: PAGINATION_RESULTS,
+	});
+
+	return <PostFeed initialPosts={posts} />;
+};
+
+export default CustomFeed;
