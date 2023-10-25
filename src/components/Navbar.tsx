cf9fdebd4d6a90b2bf6b9c280f2c05487bfeb3ca
Shahbaz <sc@shahbazc.com>
2023-10-25 13:30:56 +0100

    css changes

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -9,7 +9,7 @@ const Navbar = async () => {
 	const session = await getAuthSession();
 
 	return (
-		<div className='fixed top-0 inset-x-0 h-fit bg-gray-900 border-b border-zinc-800 z-[10] py-2'>
+		<div className='fixed top-0 inset-x-0 bg-gray-900 border-b border-zinc-800 z-[10] py-2'>
 			<div className='container max-w-7xl h-full mx-auto flex items-center justify-between gap-2'>
 				{/* LOGO */}
 				<Link href='/' className='flex gap-2 items-center'>
