1182e8a2fe553c8e91e94486fdad74e65ba9677a
Shahbaz <sc@shahbazc.com>
2023-10-25 18:48:31 +0100

    minor change

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,7 +13,9 @@ export default async function Home() {
 
 	return (
 		<>
-			<h1 className='font-bold text-3xl md:text-4xl'>Your Feed</h1>
+			<h1 className='font-bold text-3xl md:text-4xl'>
+				{session ? 'Your Feed' : 'Feed'}
+			</h1>
 			<div className='grid grid-cols-1 md:grid-cols-3 gap-y-4 md:gap-x-4 py-6'>
 				{/* FEED */}
 				{/* @ts-expect-error server component */}
