e4e6824859cbf2887162f5152f4d9d532701ad5f
Shahbaz <sc@shahbazc.com>
2023-10-25 19:56:39 +0100

    css changes

--- src/app/r/[slug]/post/[postId]/page.tsx
+++ src/app/r/[slug]/post/[postId]/page.tsx
@@ -43,8 +43,8 @@ const page = async ({ params }: PageProps) => {
 
 	return (
 		<div>
-			{/* <div className='h-full flex flex-col sm:flex-row items-center sm:items-start justify-between'> */}
-			<div className='px-6 py-4 flex justify-between'>
+			<div className='h-full flex flex-row sm:flex-row items-center sm:items-start justify-between'>
+				{/* <div className='px-6 py-4 flex justify-between'> */}
 				<Suspense fallback={<PostVoteShell />}>
 					{/* @ts-expect-error server component */}
 					<PostVoteServer
